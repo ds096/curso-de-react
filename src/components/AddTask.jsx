062ae69504324b72d1766df2fe16943aba42a10a
ds096 <ss.daniel096@gmail.com>
2026-05-14 08:46:53 -0300

    creat components: Tasks and AddTask

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,11 @@
+function AddTask() {
+    return (
+        <div>
+            <h2>Adicionar Tarefa</h2>
+            <input type="text" placeholder="Digite a tarefa" />
+            <button>Adicionar</button>
+        </div>
+    )
+}
+
+export default AddTask
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+function Tasks() {
+    return (
+        <div>
+            <h2>Lista de Tarefas</h2>
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa 2</li>
+                <li>Tarefa 3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Tasks
