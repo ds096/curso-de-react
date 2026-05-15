4711cd4351c9697c633e611f2185d6d02958ce01
ds096 <ss.daniel096@gmail.com>
2026-05-15 16:30:32 -0300

    create TaskPage

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,17 +1,46 @@
-import { useSearchParams } from "react-router-dom"
+import { ChevronLeftIcon } from "lucide-react";
+import { useSearchParams, useNavigate } from "react-router-dom"
 
 function TaskPage() {
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const taskTitle = searchParams.get("title");
     const taskDescription = searchParams.get("description");
     const taskCompleted = searchParams.get("completed");
 
     return (
-        <div
-            className="h-screen w-full bg-slate-500 p-6">
-            <p>{taskTitle}</p>
-            <p>{taskDescription}</p>
-            <p>Status: {taskCompleted ? "Concluída" : "Pendente"}</p>
+        <div className='w-full h-screen justify-center p-6 bg-slate-500 '>
+            <div className='space-y-4'>
+                <div className="flex justify-center relative mb-6">
+                    <button
+                        className=" absolute left-0 top-0 bottom-0"
+                        onClick={() => { navigate(-1) }}>
+                        <ChevronLeftIcon />
+                    </button>
+                    <h1
+                        className='text-3xl text-slate-100 font-bold p-2 text-center'>
+                        Detalhes da Tarefa
+                    </h1>
+                </div>
+
+                <div className="text-left bg-slate-200 p-4 rounded-md">
+                    <h2 className="text-xl font-bold text-slate-600">
+                        {taskTitle}
+                    </h2>
+                    <p className="text-slate-700 mt-2">
+                        Descrição: {taskDescription}
+                    </p>
+                    <p className="text-slate-700 mt-2">
+                        Status: {taskCompleted === "true" ? "Concluída" : "Pendente"}
+                    </p>
+                </div>
+
+            </div>
+
+
+
+
+
 
         </div>
     )
